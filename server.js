5929ab0d5ba71dd727efc597d47c037a90645b7f
Brandon Chang <karunashi@gmail.com>
2017-05-06 03:27:45 -0700

    Functionality enabled to an extent after numerous testing. Fixed case-sensitive variables, especially PORT, to ensure connection issues won't occur. Routing, Model, Controller and View mostly done. Need to add into views a submit+input for adding new items

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 9001;
+var PORT = process.env.PORT || 9001;
 
 var app = express();
 
@@ -25,4 +25,4 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(PORT);
